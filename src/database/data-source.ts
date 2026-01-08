29c70d2c608328733a3bc7a28ec97b4ebd07ed31
Tim Kraken <tim.moyence@gmail.com>
2026-01-08 17:13:21 +0100

    update typeorm connection

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -8,14 +8,14 @@ export default new DataSource({
   host: process.env.DB_HOST ?? process.env.PGHOST,
   port: Number(process.env.DB_PORT ?? process.env.PGPORT ?? 5432),
   username:
-    process.env.DB_USERNAME ?? process.env.DB_USER ?? process.env.PGUSER,
+    process.env.DB_USERNAME ?? process.env.DB_USER ?? process.env.POSTGRES_USER,
   password:
     process.env.DB_PASSWORD ??
     process.env.DATABASE_PASSWORD ??
-    process.env.PGPASSWORD,
+    process.env.POSTGRES_PASSWORD,
   database:
     process.env.DB_NAME ??
-    process.env.PGDATABASE ??
+    process.env.POSTGRES_DB ??
     process.env.DB_DATABASE ??
     process.env.DATABASE_NAME,
   entities: ['dist/**/*.entity.js'],
